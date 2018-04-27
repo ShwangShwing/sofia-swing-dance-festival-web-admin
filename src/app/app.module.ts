dabd5d043d469849df63a9312172866a9e78b625
ShwangShwing <gitonchev@gmail.com>
2018-04-27 18:59:48 +0300

    added some routing and experimenting with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+
+import { PublicModule } from './public/public.module'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
