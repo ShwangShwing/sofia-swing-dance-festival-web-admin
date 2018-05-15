0e70ba1633f3846df939278c30a5c09f56fb8231
ShwangShwing <gitonchev@gmail.com>
2018-05-16 01:41:17 +0300

    fixed ids of venues and instructors since they include the current ssdf year when copying ssdf years

--- src/app/services/data/ssdf-years.service.ts
+++ src/app/services/data/ssdf-years.service.ts
@@ -16,14 +16,14 @@ export class SsdfYearsService {
         this.isSelectedSsdfYearInit = true;
       }
     });
-   }
+  }
 
   getAll(): Observable<string[]> {
     return this.af.list('/').snapshotChanges()
       .map((ssdfYears) => {
         return ssdfYears.map(ssdfYear => ssdfYear.key)
-        .filter(ssdfYear => ssdfYear !== 'activeSsdfYear')
-        .sort((left, right) => right > left ? 1 : 0);
+          .filter(ssdfYear => ssdfYear !== 'activeSsdfYear')
+          .sort((left, right) => right > left ? 1 : 0);
       });
   }
 
@@ -52,7 +52,7 @@ export class SsdfYearsService {
       .then(year => {
         if (!year) {
           this.af.object(`/${ssdfYearName}`)
-            .update({creationMethod: 'Created de novo by web admin'});
+            .update({ creationMethod: 'Created de novo by web admin' });
         }
       });
   }
@@ -69,6 +69,28 @@ export class SsdfYearsService {
             .first()
             .toPromise<any>()
             .then(oldYear => {
+              const events = oldYear.events || {};
+              for (const eventKey in events) {
+                if (events.hasOwnProperty(eventKey)) {
+                  const event = events[eventKey];
+                  if (typeof event.venueId === 'string') {
+                    event.venueId =
+                      event.venueId.replace(ssdfYearFrom, ssdfYearName);
+                  }
+
+                  const instructorIds = event.instructorIds || {};
+                  for (const instructorKey in instructorIds) {
+                    if (instructorIds.hasOwnProperty(instructorKey)) {
+                      const instructorId = instructorIds[instructorKey];
+                      if (typeof instructorId === 'string') {
+                        instructorIds[instructorKey] =
+                          instructorId.replace(ssdfYearFrom, ssdfYearName);
+                      }
+                    }
+                  }
+                }
+              }
+
               oldYear.creationMethod = `Copied from ${ssdfYearFrom} by web admin`;
               this.af.object(`/${ssdfYearName}`)
                 .update(oldYear);
