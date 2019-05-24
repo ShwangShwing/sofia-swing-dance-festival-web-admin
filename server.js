0aeacae23f14f6d5ed1d591cdc62eb8b6f822adb
ShwangShwing <gitonchev@gmail.com>
2019-05-24 15:54:18 +0300

    disabled http on heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+var forceSsl = require('force-ssl-heroku');
 
 const app = express();
 
 // Serve only the static files form the dist directory
+app.use(forceSsl);
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
